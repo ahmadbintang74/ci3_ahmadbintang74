bd05e041f3cdf1031a6c477cd945b430bce63615
ahmadbitang74 <tangjul60@gmail.com>
2023-10-01 23:10:11 +0700

    website dengan/tanpa css ges:(

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -4,12 +4,20 @@ defined('BASEPATH') or exit ('no direct script access allowed');
 class Web extends CI_Controller{
     function __contruct(){
         parent::__contruct();
+        $this->load->helper('url');
     }
-}
 
-public function index(){
-    $data['judul'] = "halaman depan";
-    $this->load->view('v_header.php',$data);
-    $this->load->view('v_index.php',$data);
-    $this->load->view('v_footer.php',$data);
+    public function index(){
+        $data['judul'] = "Halaman Depan";
+        $this->load->view('v_header.php',$data);
+        $this->load->view('v_index.php',$data);
+        $this->load->view('v_footer.php',$data);
+    }
+
+    public function about(){
+        $data['judul'] = "Halaman About";
+        $this->load->view('v_header.php',$data);
+        $this->load->view('v_about.php',$data);
+        $this->load->view('v_footer.php',$data);
+    }
 }
